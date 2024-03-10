4c7a1f1f5e689b9d041ecae59bb377c800a69d38
ghifrank <ayoub3412@gmail.com>
2024-03-10 14:59:27 +0000

    y

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Base Model"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """defines a BaseModel"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns printable string representation"""
+        return "[{}] ({}) {}".format(BaseModel.__name__,self.id,self.__dict__)
+
+    def save(self):
+        """updates updated_at with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        instance_dict = self.__dict__.copy()
+
+        instance_dict['__class__'] = self.__class__.__name__
+
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+
+        return instance_dict
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
